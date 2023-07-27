19d408350d47d72a2d8290e78e09b3f237141d93
Tthh123 <gtthh123@gmail.com>
2023-07-27 22:11:10 +0800

    Clean up change var names etc

--- Client.py
+++ Client.py
@@ -2,14 +2,13 @@ import json
 from tkinter import filedialog
 import threading
 from signal import SIGTERM
-from os import system
-from os import kill
-from os import getpid
+import os
 import ipaddress
 import socket
 
-class Client :
-    def __init__(self) :
+
+class Client:
+    def __init__(self):
         self.running = True
         self.messages = []
         self.send_port = 8081
@@ -19,107 +18,110 @@ class Client :
         self.host, self.port = self.get_host_port()
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.host, self.port))
-        resp = self.client.recv(1024).decode()
+        self.client.recv(1024).decode()
 
-        while self.running :
+        while self.running:
             self.name = input(f"Enter Client's name : ").strip().split()
             self.name = '_'.join(self.name)
             self.client.send(self.name.encode())
             resp = self.client.recv(1024).decode()
             resp = self.decode(resp)[0]
-            print(resp)
-            if resp == 'Ok':
+            if resp == 'connected':
                 break
-            else : 
+            else:
                 print('Please Try Again')
                 continue
-        
+
         server_name = self.client.recv(1024).decode()
         server_name = self.decode(server_name)[0]
         self.log_message(f"\n{server_name} has joined the chat")
         self.session()
-        
-    def save_img(self, s) : 
+
+    def save_img(self, s):
         resp = s.recv(1024).decode()
         resp = self.decode(resp)[0]
-        if resp : 
+        if resp:
             json_msg = json.loads(resp)
-            if json_msg['status'] == 'Ok' : 
-                filename = filedialog.asksaveasfilename() 
-                try : 
-                    with open(filename, 'wb') as f :
+            if json_msg['file_name'] == 'img':
+                file_types = [
+                    ("All Files", "*.*")
+                ]
+                filename = filedialog.asksaveasfilename(filetypes=file_types)
+                try:
+                    with open(filename, 'wb') as f:
                         data = b''
-                        while self.running :
+                        while self.running:
                             cdata = s.recv(1024)
-                            if cdata[-4:] != b'AAAA':                          
+                            if cdata[-4:] != b'work':
                                 data += cdata
-                            else :
+                            else:
                                 f.write(data)
-                                self.log_message('Download complete')
+                                self.log_message('Download complete!')
                                 s.close()
                                 break
                 except Exception as e:
-                    self.log_message("Error while downloading image", e)
+                    self.log_message("Error!", e)
 
-            else : 
-                print(f'Error\nTip: Plase check the filename')
+            else:
+                print(f'No such file!')
                 return
-            
-    def GracExit(self) : 
-        pid = getpid()
-        kill(pid, SIGTERM)
 
-    def decode(self, msg) : 
+    def exit(self):
+        pid = os.getpid()
+        os.kill(pid, SIGTERM)
+
+    def decode(self, msg):
         msg = msg.split("\x00\x00\x00\x00")
         return msg
-    def get_json(self, msg) : 
+
+    def get_json(self, msg):
         res = []
-        for j in msg : 
-            if j :
-                try : 
+        for j in msg:
+            if j:
+                try:
                     json_msg = json.loads(j)
                     res.append(json_msg)
                     continue
-                except :
+                except:
                     print("invalid json message ", j)
                     pass
         return res
 
-    def recv_msg(self) :
-        while self.running :
+    def recv_msg(self):
+        while self.running:
             msg = self.client.recv(1024).decode()
             msg = self.decode(msg)
-            if msg : 
+            if msg:
                 json_msgs = self.get_json(msg)
-                for json_msg in json_msgs :
-                    # check for possible commands 
+                for json_msg in json_msgs:
+                    # check for possible commands
                     if self.name in json_msg['data']:
                         continue
 
                     elif "has joined the chat" in json_msg['data']:
-                        system('cls')
+                        os.system('cls' if os.name == 'nt' else 'clear')
                         self.log_message(f"{json_msg['data']}")
-                        print(f"\n{self.name} : " , end='')
+                        print(f"\n{self.name} : ", end='')
                         continue
 
                     elif "has left the chat" in json_msg['data']:
-                        system('cls')
+                        os.system('cls' if os.name == 'nt' else 'clear')
                         self.log_message(f"{json_msg['data']}")
-                        print(f"\n{self.name} : " , end='')
+                        print(f"\n{self.name} : ", end='')
                         continue
 
-                    elif json_msg.get('kill') : 
+                    elif json_msg.get('end'):
                         self.log_message("Server has been closed")
-                        self.GracExit()
+                        self.exit()
 
-                    system('cls')
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     self.log_message(f"{json_msg['name']} : {json_msg['data']}")
-                    print(f"\n{self.name} : " , end='')
+                    print(f"\n{self.name} : ", end='')
 
-    def fetch_image(self, cmd) :
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    def fetch_image(self, cmd):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.host, self.image_port))
-        s.send(json.dumps({"name" : self.name, "data" : cmd}).encode())
+        s.send(json.dumps({"name": self.name, "data": cmd}).encode())
 
         self.save_img(s)
 
@@ -127,26 +129,25 @@ class Client :
         self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.send_socket.connect((self.host, self.send_port))
         threading.Thread(target=self.recv_msg).start()
-        while self.running :
+        while self.running:
             msg = input(f"\n{self.name} : ")
             print(end='')
-            if msg : 
-                self.send_socket.send(json.dumps({"name" : self.name, "data" : msg}).encode())
+            if msg:
+                self.send_socket.send(json.dumps({"name": self.name, "data": msg}).encode())
                 data = msg.strip().split()
-                if len(data) == 2 : 
+                if len(data) >= 2:
                     if data[0].lower() == 'download':
                         self.fetch_image(msg)
                         continue
-                if data[0].lower() == 'bye' : 
-                        self.client.close()
-                        self.send_socket.close()
-                        self.GracExit()
+                if data[0].lower() == 'bye':
+                    self.client.close()
+                    self.send_socket.close()
+                    self.exit()
 
-
-    def get_host_port(self) :
-        while self.running :
-            data=input("Enter chat server's IP address and port:")
-            if len(data.split())>1:
+    def get_host_port(self):
+        while self.running:
+            data = input("Enter chat server's IP address and port:")
+            if len(data.split()) > 1:
                 host, port = data.split()
                 try:
                     ipaddress.ip_address(host)
@@ -158,17 +159,17 @@ class Client :
                     if (int(port) != self.send_port) and (int(port) != self.image_port):
                         return host, int(port)
                     else:
-                        print(f'The port {port} is reserved. ')
+                        print(f'The port at {port} is reserved. ')
                         continue
-                print(f'enter valid data,\ntry again...')
+                print(f'Enter valid data,\ntry again...')
             else:
                 print(f'Please enter a valid IP address and port!')
 
-    def log_message(self, msg) :
+    def log_message(self, msg):
         self.messages.append(msg)
-        system('cls')
+        os.system('cls' if os.name == 'nt' else 'clear')
         for i in self.messages: print(i)
 
 
-if __name__ == '__main__' : 
-    Client() 
+if __name__ == '__main__':
+    Client()
--- Server.py
+++ Server.py
@@ -1,16 +1,13 @@
 import json
 import threading
-from os import listdir
-from signal import SIGTERM
-from os import system
-from os import getpid
-from os import kill
 import socket
-import re
+import os
+from signal import SIGTERM
 
 
 class Server:
     def __init__(self):
+        # Initialization
         self.messages = []
         self.running = True
         self.img_port = 8082
@@ -18,13 +15,12 @@ class Server:
         self.recv_server = True
         self.client_commands = {"list", "bye"}
         self.mod_commands = {"rename", "bye"}
-        self.clients: dict = {}
+        self.clients = {}
         self.log_message(f'Hello, this is INF1006 Chat server, please enter the listening port:')
-        self.ip,self.port = self.get_port()
+        self.server_ip_adr, self.server_port = self.get_port()
         self.name = self.get_name()
-
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.ip, self.port))
+        self.server.bind((self.server_ip_adr, self.server_port))
         self.server.listen()
 
         self.log_message("Waiting for incoming connections...")
@@ -32,14 +28,13 @@ class Server:
             conn, addr = self.server.accept()
             ip, pid = map(str, addr)
             self.log_message("\nReceived connection from " + ip + ", (" + pid + ")")
-            self.log_message("Connection Established. Connected From: "+ip+", ("+pid+")")
+            self.log_message("Connection Established. Connected From: " + ip + ", (" + pid + ")")
 
-            conn.send(json.dumps({"name": self.name, "data": "Welcome to the server" + "\0\0\0\0"}).encode())
+            conn.send(json.dumps({"name": self.name, "data": "Connection success" + "\0\0\0\0"}).encode())
 
             threading.Thread(target=self.session, args=(conn,)).start()
             if self.recv_server:
                 threading.Thread(target=self.recv_msg).start()
-
 
     def get_name(self) -> str:
         while True:
@@ -49,6 +44,7 @@ class Server:
     def send_all(self, data, ignore=""):
         for client in self.clients:
             if ignore and client == ignore:
+                # Don't send to server
                 continue
             self.clients[client].send((json.dumps(data) + "\0\0\0\0").encode())
 
@@ -57,10 +53,9 @@ class Server:
         print(client)
         client.send((json.dumps(data) + "\0\0\0\0").encode())
 
-
-    def image_server(self):
-        def serve(conn):
-            json_data = conn.recv(1024).decode()
+    def image_handling(self):
+        def serve(connection):
+            json_data = connection.recv(1024).decode()
             if json_data:
                 json_data = json.loads(json_data)
                 command = json_data['data']
@@ -69,51 +64,51 @@ class Server:
                 if len(data) == 2:
                     filename = data[1]
 
-                    if filename in listdir('./server-files/images'):
-                        conn.send((json.dumps(
-                            {"name": self.name, "data": "File Found", "status": "Ok"}) + "\0\0\0\0").encode())
+                    if filename in os.listdir('images'):
+                        connection.send((json.dumps(
+                            {"name": self.name, "data": "Image file", "file_name": "img"}) + "\0\0\0\0").encode())
 
-                        with open(f'./server-files/images/{filename}', 'rb') as f:
+                        with open(f'images/{filename}', 'rb') as f:
                             image_data = f.read()
-                        conn.sendall(image_data)
-                        conn.send(b"AAAA")
+                        connection.sendall(image_data)
+                        connection.send(b"work")
 
                     else:
 
-                        conn.send((json.dumps(
-                            {"name": self.name, "data": "File Not Found ", "status": "Err"}) + "\0\0\0\0").encode())
+                        connection.send((json.dumps(
+                            {"name": self.name, "data": "File Not Found ", "status": "error"}) + "\0\0\0\0").encode())
 
                 else:
-                    conn.send(json.dumps(({"name": self.name, "data": "Syntax error in the command",
-                                           "status": "Err"}) + "\0\0\0\0").encode())
+                    connection.send(json.dumps(({"name": self.name, "data": "Syntax error in the command",
+                                                 "status": "Err"}) + "\0\0\0\0").encode())
                     return
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.ip, self.img_port))
+        s.bind((self.server_ip_adr, self.img_port))
         s.listen()
 
         while True:
-            conn, _ = s.accept()
+            conn, addr = s.accept()
             threading.Thread(target=serve, args=(conn,)).start()
 
     def recv_msg(self):
         self.recv_server = False
-        threading.Thread(target=self.image_server).start()
+        threading.Thread(target=self.image_handling).start()
 
-        def recv_fn(conn):
+        def recv_fn(connection):
             while True:
-                msg = conn.recv(1024).decode()
+                msg = connection.recv(1024).decode()
                 if msg:
                     try:
                         json_msg = json.loads(msg)
                     except:
-                        print("cannot convert msg to json", msg)
+                        print("Error", msg)
                         continue
 
                     _command = json_msg['data'].strip().lower().split()
                     if _command[0].lower() in self.client_commands:
                         if _command[0] == 'list':
-                            images: list = listdir('./server-files/images')
+                            images: list = os.listdir('images')
                             self.send(self.clients[json_msg['name']], {"name": self.name, "data": images})
                             self.log_message(
                                 f"{json_msg['name']} :{json_msg['data']}")
@@ -123,43 +118,43 @@ class Server:
                             continue
 
                         elif _command[0] == 'bye':
-                            self.clients[json_msg['name']].close()
-                            del self.clients[json_msg['name']]
-                            self.send_all({"name": json_msg['name'], "data": "bye"})
+                            client_name = json_msg['name']
+                            self.clients[client_name].close()
+                            del self.clients[client_name]
+                            self.send_all({"name": client_name, "data": "bye"})
                             self.log_message(
-                                f"{json_msg['name']} : {json_msg['data']}") 
-                            self.send_all({"name": self.name, "data": f"<{json_msg['name']}> has left the chat"})
+                                f"{client_name} : {json_msg['data']}")
+                            self.send_all({"name": self.name, "data": f"<{client_name}> has left the chat"})
                             print(f"\n{self.name} :", end='')
                             continue
 
                 else:
                     continue
 
-                system('cls')
-                self.log_message( 
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.log_message(
                     f"{json_msg['name']} : {json_msg['data']}")
                 self.send_all({"name": f"{json_msg['name']}", "data": f"{json_msg['data']}"})
                 print(f"\n{self.name} :", end='')
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.ip, self.recv_port))
+        s.bind((self.server_ip_adr, self.recv_port))
         s.listen()
 
         while True:
             conn, _ = s.accept()
             threading.Thread(target=recv_fn, args=(conn,)).start()
 
-
-    def GracExit(self):
-        pid = getpid()
-        kill(pid, SIGTERM)
+    def exit(self):
+        pid = os.getpid()
+        os.kill(pid, SIGTERM)
 
     def session(self, conn):
         name = conn.recv(1024).decode()
         self.log_message(name + " has joined the chat.")
         while self.running:
             if not (name in self.clients):
-                conn.send(("Ok" + "\0\0\0\0").encode())
+                conn.send(("connected" + "\0\0\0\0").encode())
                 break
             conn.send(("Error" + "\0\0\0\0").encode())
             name = conn.recv(1024).decode()
@@ -173,10 +168,10 @@ class Server:
 
             if msg:
                 instruction = msg.strip().split()
-                if instruction[0] == 'close':
-                    self.send_all({"name": self.name, "data": "Server Closed", 'kill': True})
+                if instruction[0] == 'quit':
+                    self.send_all({"name": self.name, "data": "Server Closed", 'end': True})
                     print(f"Server Closed...\nExiting...")
-                    self.GracExit()
+                    self.exit()
                 for client in self.clients:
                     self.clients[client].send((json.dumps({"name": self.name, "data": msg}) + "\0\0\0\0").encode())
 
@@ -201,15 +196,16 @@ class Server:
                     except:
                         self.log_message("Invalid port, please try again.")
                         continue
-                    return [ip,int(port)]
+                    return [ip, int(port)]
                 else:
                     self.log_message("Reserved port, please try again.")
                     continue
 
     def log_message(self, msg=""):
         self.messages.append(msg)
-        system('cls')
+        os.system('cls' if os.name == 'nt' else 'clear')
         for i in self.messages: print(i)
+
 
 if __name__ == '__main__':
     Server()
